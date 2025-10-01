3406ae6b43b1cd24bfc013645ece7ae2cb1909c1
nishino-yoichiro <yoichironishino@gmail.com>
2025-10-01 17:15:28 -0500

    updated dependencies

--- data_ingestion/__init__.py
+++ data_ingestion/__init__.py
@@ -17,11 +17,42 @@ Key Features:
 
 from .news_apis import NewsAPIClient, RavenPackClient
 from .price_apis import PolygonClient, AlpacaClient, BinanceClient, CoinGeckoClient
-from .coinbase_client import CoinbaseClient, CoinbaseConfig, create_coinbase_client
-from .coinbase_rest_client import CoinbaseRESTClient, create_coinbase_rest_client
-from .social_apis import RedditClient, TwitterClient
 from .data_validator import DataValidator
 from .unified_collector import UnifiedDataCollector
+
+# Optional imports for exchange clients (may not be available in all environments)
+try:
+    from .coinbase_client import CoinbaseClient, CoinbaseConfig, create_coinbase_client
+    from .coinbase_rest_client import CoinbaseRESTClient, create_coinbase_rest_client
+    from .social_apis import RedditClient, TwitterClient
+    _HAS_EXCHANGE_CLIENTS = True
+except ImportError:
+    _HAS_EXCHANGE_CLIENTS = False
+    # Create dummy classes for when exchange clients are not available
+    class CoinbaseClient:
+        def __init__(self, *args, **kwargs):
+            raise ImportError("Coinbase client not available")
+    
+    class CoinbaseConfig:
+        pass
+    
+    def create_coinbase_client(*args, **kwargs):
+        raise ImportError("Coinbase client not available")
+    
+    class CoinbaseRESTClient:
+        def __init__(self, *args, **kwargs):
+            raise ImportError("Coinbase REST client not available")
+    
+    def create_coinbase_rest_client(*args, **kwargs):
+        raise ImportError("Coinbase REST client not available")
+    
+    class RedditClient:
+        def __init__(self, *args, **kwargs):
+            raise ImportError("Reddit client not available")
+    
+    class TwitterClient:
+        def __init__(self, *args, **kwargs):
+            raise ImportError("Twitter client not available")
 
 __all__ = [
     'NewsAPIClient',
